2b93b359c7a0b8b4a35c7e7d25625572db755edb
Azamat <tuzelbajazamat361@gmail.com>
2025-02-17 13:44:21 +0500

    Changes

--- scripts/products.js
+++ scripts/products.js
@@ -1,10 +1,14 @@
 
+const cardSection = document.getElementById('cardSection');
+let gamesArr = [];
+
 async function getData() {
     try {
         const response = await fetch('./scripts/games.json');
         const data = await response.json();
-
-        console.log(data);
+        
+        gamesArr = data.games
+        displayGames(gamesArr)
         
     } catch (error) {
         console.error('Data is not working', error);
@@ -12,3 +16,31 @@ async function getData() {
 }
 
 getData();
+
+
+function displayGames(gamesArr) {
+    
+    gamesArr.forEach((game) => {
+        
+        const card = document.createElement('div');
+        const cardImage = document.createElement('img');
+        const gameTitle = document.createElement('h1');
+        const textCard = document.createElement('div');
+        const description = document.createElement('p');
+
+        description.textContent = game.minString
+        textCard.classList.add('textCard')
+        card.classList.add('gameCard')
+        gameTitle.textContent = game.title;
+        cardImage.src = game.image
+
+        card.appendChild(cardImage);
+        textCard.appendChild(gameTitle);
+        textCard.appendChild(description)
+        card.appendChild(textCard);
+
+        cardSection.appendChild(card);
+
+        
+    })
+}
